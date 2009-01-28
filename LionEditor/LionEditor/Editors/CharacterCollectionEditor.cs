70aafaade1de43d8bc979650325d6d2b43d58d23
joedavidson <joedavidson@8d881270-3eb2-11de-9146-fdf1833cc731>
2009-01-28 02:50:20 +0000

    Create tags for LionEditor milestones

--- LionEditor/LionEditor/Editors/CharacterCollectionEditor.cs
+++ LionEditor/LionEditor/Editors/CharacterCollectionEditor.cs
@@ -323,11 +323,7 @@ namespace LionEditor
 
             characterSelector.Items[characterSelector.SelectedIndex] = notMoved;
             characterSelector.Items[characterSelector.SelectedIndex + add] = moved;
-            
             characterSelector.SelectedItem = moved;
-
-            CharacterCollection[moved.Index] = moved;
-            CharacterCollection[notMoved.Index] = notMoved;
 
             FireDataChangedEvent();
             this.ResumeLayout();

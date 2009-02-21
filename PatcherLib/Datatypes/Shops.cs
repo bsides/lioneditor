7ba433a2834d425fb3e6a13960349c7df9edce95
joedavidson <joedavidson@8d881270-3eb2-11de-9146-fdf1833cc731>
2009-02-21 00:34:06 +0000

    Release of TacText rewrite b3

---
+++ PatcherLib/Datatypes/Shops.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PatcherLib.Datatypes
+{
+    [Flags]
+    public enum Shops
+    {
+        Lesalia = 0x8000,
+        Riovanes = 0x4000,
+        Igros = 0x2000,
+        Lionel = 0x1000,
+        Limberry = 0x0800,
+        Zeltennia = 0x0400,
+        Gariland = 0x0200,
+        Yardrow = 0x0100,
+        Goland = 0x0080,
+        Dorter = 0x0040,
+        Zaland = 0x0020,
+        Goug = 0x0010,
+        Warjilis = 0x0008,
+        Bervenia = 0x0004,
+        Zarghidas = 0x0002,
+        None = 0x0001,
+        Empty = 0
+    }
+}
--- utils/isopatcherv05/src/iso_patch.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ISO_PATCH_H
-#define _ISO_PATCH_H
-
-int WINAPI iso_patch_file(int iso_type, char *iso_file, int need_ecc_edc, int iso_offset, char *patch_file);
-int WINAPI iso_patch_byte(int iso_type, char *iso_file, int need_ecc_edc, int iso_offset, char *patch_buf, int patch_bytes);
-int WINAPI iso_patch_list(int iso_type, char *iso_file, int need_ecc_edc, char *patch_list_file);
-
-#endif
